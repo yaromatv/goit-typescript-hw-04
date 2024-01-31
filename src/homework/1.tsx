d776262caebc04c53f5f1ffba53be82ae504bee7
yaromatv <122043732+yaromatv@users.noreply.github.com>
2024-01-31 21:32:05 +0100

    done

--- src/homework/1.tsx
+++ src/homework/1.tsx
@@ -1,40 +1,44 @@
-import React, { useEffect, useRef } from 'react';
+import React, { ReactNode, useEffect, useRef } from "react";
 
+type Props = {
+    children: ReactNode;
+    onContentEndVisible: () => void;
+};
 // Опишіть Props
 export function Observer({ children, onContentEndVisible }: Props) {
-  // Вкажіть правильний тип для useRef зверніть увагу, в який DOM елемент ми його передаємо
-  const endContentRef = useRef(null);
+    // Вкажіть правильний тип для useRef зверніть увагу, в який DOM елемент ми його передаємо
+    const endContentRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    // Вкажіть правильний тип для options, підказка, клас також можна вказувати як тип
-    const options = {
-      rootMargin: '0px',
-      threshold: 1.0,
-      root: null,
-    };
+    useEffect(() => {
+        // Вкажіть правильний тип для options, підказка, клас також можна вказувати як тип
+        const options: IntersectionObserverInit = {
+            rootMargin: "0px",
+            threshold: 1.0,
+            root: null,
+        };
 
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.intersectionRatio > 0) {
-          onContentEndVisible();
-          observer.disconnect();
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (entry.intersectionRatio > 0) {
+                    onContentEndVisible();
+                    observer.disconnect();
+                }
+            });
+        }, options);
+
+        if (endContentRef.current) {
+            observer.observe(endContentRef.current);
         }
-      });
-    }, options);
 
-    if (endContentRef.current) {
-      observer.observe(endContentRef.current);
-    }
+        return () => {
+            observer.disconnect();
+        };
+    }, [onContentEndVisible]);
 
-    return () => {
-      observer.disconnect();
-    };
-  }, [onContentEndVisible]);
-
-  return (
-    <div>
-      {children}
-      <div ref={endContentRef} />
-    </div>
-  );
+    return (
+        <div>
+            {children}
+            <div ref={endContentRef} />
+        </div>
+    );
 }
--- src/homework/4.tsx
+++ src/homework/4.tsx
@@ -1,91 +1,105 @@
-import React, { createContext, useMemo, useState, useContext } from "react";
+import React, {
+    createContext,
+    useMemo,
+    useState,
+    useContext,
+    ReactNode,
+} from "react";
 import noop from "lodash/noop";
 
 type MenuIds = "first" | "second" | "last";
 type Menu = { id: MenuIds; title: string };
+type SelectedMenu = { id: MenuIds };
+type MenuSelected = { selectedMenu: SelectedMenu };
+type MenuAction = { onSelectedMenu: (selectedMenu: SelectedMenu) => void };
 
 // Додати тип Menu Selected
 
 const MenuSelectedContext = createContext<MenuSelected>({
-  selectedMenu: {},
+    selectedMenu: {} as SelectedMenu,
 });
 
 // Додайте тип MenuAction
 
 const MenuActionContext = createContext<MenuAction>({
-  onSelectedMenu: noop,
+    onSelectedMenu: noop,
 });
 
 type PropsProvider = {
-  children; // Додати тип для children
+    children: ReactNode; // Додати тип для children
 };
 
 function MenuProvider({ children }: PropsProvider) {
-  // Додати тип для SelectedMenu він повинен містити { id }
-  const [selectedMenu, setSelectedMenu] = useState<SelectedMenu>({});
+    // Додати тип для SelectedMenu він повинен містити { id }
+    const [selectedMenu, setSelectedMenu] = useState<SelectedMenu>(
+        {} as SelectedMenu
+    );
 
-  const menuContextAction = useMemo(
-    () => ({
-      onSelectedMenu: setSelectedMenu,
-    }),
-    []
-  );
+    const menuContextAction = useMemo(
+        () => ({
+            onSelectedMenu: setSelectedMenu,
+        }),
+        []
+    );
 
-  const menuContextSelected = useMemo(
-    () => ({
-      selectedMenu,
-    }),
-    [selectedMenu]
-  );
+    const menuContextSelected = useMemo(
+        () => ({
+            selectedMenu,
+        }),
+        [selectedMenu]
+    );
 
-  return (
-    <MenuActionContext.Provider value={menuContextAction}>
-      <MenuSelectedContext.Provider value={menuContextSelected}>
-        {children}
-      </MenuSelectedContext.Provider>
-    </MenuActionContext.Provider>
-  );
+    return (
+        <MenuActionContext.Provider value={menuContextAction}>
+            <MenuSelectedContext.Provider value={menuContextSelected}>
+                {children}
+            </MenuSelectedContext.Provider>
+        </MenuActionContext.Provider>
+    );
 }
 
 type PropsMenu = {
-  menus; // Додайте вірний тип для меню
+    menus: Menu[]; // Додайте вірний тип для меню
 };
 
 function MenuComponent({ menus }: PropsMenu) {
-  const { onSelectedMenu } = useContext(MenuActionContext);
-  const { selectedMenu } = useContext(MenuSelectedContext);
+    const { onSelectedMenu } = useContext(MenuActionContext);
+    const { selectedMenu } = useContext(MenuSelectedContext);
 
-  return (
-    <>
-      {menus.map((menu) => (
-        <div key={menu.id} onClick={() => onSelectedMenu({ id: menu.id })}>
-          {menu.title}{" "}
-          {selectedMenu.id === menu.id ? "Selected" : "Not selected"}
-        </div>
-      ))}
-    </>
-  );
+    return (
+        <>
+            {menus.map((menu) => (
+                <div
+                    key={menu.id}
+                    onClick={() => onSelectedMenu({ id: menu.id })}
+                >
+                    {menu.title}{" "}
+                    {selectedMenu.id === menu.id ? "Selected" : "Not selected"}
+                </div>
+            ))}
+        </>
+    );
 }
 
 export function ComponentApp() {
-  const menus: Menu[] = [
-    {
-      id: "first",
-      title: "first",
-    },
-    {
-      id: "second",
-      title: "second",
-    },
-    {
-      id: "last",
-      title: "last",
-    },
-  ];
+    const menus: Menu[] = [
+        {
+            id: "first",
+            title: "first",
+        },
+        {
+            id: "second",
+            title: "second",
+        },
+        {
+            id: "last",
+            title: "last",
+        },
+    ];
 
-  return (
-    <MenuProvider>
-      <MenuComponent menus={menus} />
-    </MenuProvider>
-  );
+    return (
+        <MenuProvider>
+            <MenuComponent menus={menus} />
+        </MenuProvider>
+    );
 }
